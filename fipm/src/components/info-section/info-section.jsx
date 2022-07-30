ee1e70b9e3b1c58600b53c0b80e83a5ce1dad27a
Bhavesh Anandpara <bhaveshAP09112002@gmail.com>
2022-07-30 21:38:59 +0530

    Image Embedded Dynamically  Done

--- fipm/src/components/info-section/info-section.jsx
+++ fipm/src/components/info-section/info-section.jsx
@@ -1,26 +1,47 @@
 import React from 'react'
+import { useRef, useState, useEffect } from 'react'
+import * as ReactDOM from 'react-dom/client';
 
 export default function InfoSection(props) {
-    let imageArray = props.data.images
+
+  const imgSection = useRef()
+  const [imageArray, setImageArray] = useState(props.data.images)
+
+
+  useEffect(() => {
+
+    let images = []
+    let root = ReactDOM.createRoot(imgSection.current)
+
+    
+    imageArray.map((ele) => {
+      let element = <div className='slider-image border' style={{ backgroundImage: `url(/assets/${ele})`, backgroundSize: "cover" }} ></div>
+      images.push(element)
+    })
+    
+    root.render(
+      images
+    )
+
+  }, [imageArray])
+
+
+
+
   return (
     <>
-        <h2 className="info-section-title">
-            {props.data.title}
-          </h2>
-          <p>
-            {props.data.desc}
-          </p>
-          <div className="info-section-images ">
-            {
-              props.data.images.map(ele=>{
-                  console.log(ele)
-                })
-            }
-          </div>
-          <p>
-          {props.data.text}
-          </p>
-          <hr/>
+      <h2 className="info-section-title">
+        {props.data.title}
+      </h2>
+      <p>
+        {props.data.desc}
+      </p>
+      <div className="info-section-images " ref={imgSection}>
+      </div>
+      <p>
+        {props.data.text}
+      </p>
+      <hr />
     </>
   )
 }
--- fipm/src/pages/Home/Home.jsx
+++ fipm/src/pages/Home/Home.jsx
@@ -18,21 +18,21 @@ export default function Home() {
     title: 'Space based observation of Indian wetlands',
     desc: "India is blessed with an enormous diversity of wetland ecosystems which play a critical role in regulating carbon and hydrological cycles, resilience in climate change and land degradation, water related disasters, nutrient cycling, local economy etc. The concern for conservation and management of wetlands is increasing all over the world including India, due to burgeoning population pressure and developmental activities. Therefore, Space Applications Centre has carried out National wetland inventory and assessment-2nd cycle under SARITA (SAtellite based RIver hydrological Techniques and Application) programme of SAC, funded by Department of Space. This National wetland geo-database is developed using multi-date Resourcesat-2/2A, LISS-III datasets and with the support of many partner Institutions. Nation-wide mapping of wetlands at 1:50000 scale provides current status of wetlands, its type, extent, size, location, distribution, as well as decadal changes. Wetlands inventory statistics along with geospatial analysis has been brought out in the form of an atlas titled “Space based observation of Indian wetlands",
     text: "Click here to Know more  (PDF Size:471MB Language: English)",
-    images: []
+    images: ['header-logo.png' , 'header-logo.png' , 'header-logo.png']
   }
 
   const infoSection2 = {
     title: 'Launch of the ‘Geospatial Energy Map of India’ by Hon’ble VC, NITI Aayog on October 18, 2021',
     desc: "The Geospatial Energy Map of India was launched by Dr. Rajiv Kumar, Vice Chairman, NITI Aayog on October 18, 2021. The occasion was graced by Dr. K. Sivan, Chairman, ISRO, Shri Amitabh Kant, CEO, NITI Aayog, Dr. V.K. Saraswat, Member, NITI Aayog and Shri Nilesh M Desai, Director SAC. NITI Aayog and ISRO jointly developed the Geospatial Energy Map of India, in association with Ministry of Power, Ministry of Coal, Ministry of New and Renewable Energy, Ministry of Petroleum and Natural Gas, and Department of Atomic Energy. The website currently provides access to 27 thematic layers of energy sector in India.",
     text: "Click here to go to Geospatial Energy Map of India Click here to watch the inauguration event (YouTube)",
-    images: []
+    images: ['header-logo.png' , 'header-logo.png']
   }
 
   const infoSection3 = {
     title: 'Catastrophic Calving Events due to Collision of D28',
     desc: "",
     text: "Click here to Know more  (PDF Size:418KB Language: English)",
-    images: []
+    images: ['header-logo.png']
   }
 
 
