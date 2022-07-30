03e9c3104070452dcbe13730339438da51ae8f00
SHIVAMCHARDE <83965173+SHIVAMCHARDE@users.noreply.github.com>
2022-07-30 21:20:32 +0530

    contact_us_add

--- fipm/src/App.js
+++ fipm/src/App.js
@@ -8,6 +8,7 @@ import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home/Home';
 import Map from './pages/Map/Map';
 import About_us from './pages/About_us/About_us';
+import Contactus from './pages/contactus/Contactus';
 
 
 
@@ -20,8 +21,10 @@ function App() {
         <Route exact path="/home" element={<Home/>} ></Route>
         <Route exact path="/aboutus" element={<About_us/>} ></Route>
         <Route exact path="/probabilitymap" element={<Map/>} ></Route>
+        <Route exact path="/Contactus" element={<Contactus/>} ></Route>
       </Routes>
-    </BrowserRouter>
+    </BrowserRouter> 
+    
 </>
   );
 }
